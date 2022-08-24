c6fcff932c3aa0567b001a8cef3d874d3cf40d61
Qutluq <kootlook.applications@gmail.com>
2022-08-25 02:04:18 +0600

    feat: added spinner component

---
+++ src/components/spinner/index.tsx
@@ -0,0 +1,7 @@
+import { RingLoader } from 'react-spinners'
+
+export const Spinner = () => (
+  <div className="flex min-h-screen flex-col items-center justify-center">
+    <RingLoader color="#3989ce" />
+  </div>
+)
