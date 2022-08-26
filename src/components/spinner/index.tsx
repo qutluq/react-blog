5c82c660c5f2b67b1eaf860d9abf8c48eaa71830
Qutluq <kootlook.applications@gmail.com>
2022-08-26 23:50:26 +0600

    chore: removed spinner package

--- src/components/spinner/index.tsx
+++
@@ -1,7 +0,0 @@
-import { RingLoader } from 'react-spinners'
-
-export const Spinner = () => (
-  <div className="flex min-h-screen flex-col items-center justify-center">
-    <RingLoader color="#3989ce" />
-  </div>
-)
