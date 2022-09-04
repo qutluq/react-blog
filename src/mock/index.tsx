ea37722b5b1f479251d1b42d930e0abd0162825d
Qutluq <kootlook.applications@gmail.com>
2022-09-04 23:37:45 +0600

    chore: removed mock data and mongodb api for populating tables

--- src/mock/index.tsx
+++
@@ -1,4 +0,0 @@
-import { getBlogposts } from 'src/mock/blogposts'
-import { getUsers } from 'src/mock/users'
-
-export { getBlogposts, getUsers }
--- src/mock/users.tsx
+++
@@ -1,6 +0,0 @@
-import { User } from 'src/components/user'
-import usersJson from 'src/mock/users.json'
-
-export const getUsers = () => {
-  return usersJson as User[]
-}
