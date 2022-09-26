4b20419f8ce2b4381d7fa6e8158cddc485846d78
Qutluq <kootlook.applications@gmail.com>
2022-09-26 23:39:04 +0600

    style: changed footer bg-color

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => (
   <footer>
-    <div className="w-full px-4 sm:px-6 lg:px-8">
-      <div className="border-t border-gray-200 py-8 text-center text-sm text-gray-500 sm:text-left">
+    <div className="w-full bg-gray-800 px-4 sm:px-6 lg:px-8">
+      <div className="py-8 text-center text-sm text-white sm:text-left">
         © 2022 <a href="https://github.com/qutluq">qutluq</a>. All rights
         reserved.
       </div>
