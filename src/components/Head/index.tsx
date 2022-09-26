be5e9953275952bd73007208fb5623c4f2cc8ee8
Qutluq <kootlook.applications@gmail.com>
2022-09-26 16:45:23 +0600

    refactoring: strings in href instead of expression

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -38,24 +38,24 @@ export const Head = (meta: PropTypes) => {
       />
       <link
         rel="apple-touch-icon"
-        href={'/assets/apple-touch-icon.png'}
+        href="/assets/apple-touch-icon.png"
         key="apple"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href={'/assets/favicon-32x32.png'}
+        href="/assets/favicon-32x32.png"
         key="icon32"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href={'/assets/favicon-16x16.png'}
+        href="/assets/favicon-16x16.png"
         key="icon16"
       />
-      <link rel="icon" href={'/assets/favicon.ico'} key="favicon" />
+      <link rel="icon" href="/assets/favicon.ico" key="favicon" />
     </NextHead>
   )
 }
