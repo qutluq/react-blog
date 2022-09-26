ca0438998e954485f25b0a842bffca0e1d2712ad
Qutluq <kootlook.applications@gmail.com>
2022-09-26 18:01:24 +0600

    feat: update footer

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -43,8 +43,16 @@ const Posts = ({
         />
       </main>
 
-      <footer className="flex flex-row text-start">
-        <span>Footer info</span>
+      <footer>
+        <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+          <div className="border-t border-gray-200 py-8 text-center text-sm text-gray-500 sm:text-left">
+            <span className="block sm:inline">
+              &copy; 2022 <a href="https://github.com/qutluq">qutluq</a>
+            </span>
+            {'. '}
+            <span className="block sm:inline">All rights reserved.</span>
+          </div>
+        </div>
       </footer>
     </div>
   )
