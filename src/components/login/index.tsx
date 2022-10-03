61f5c5f8c47f3c6f523f76b4cce9aa0642e2e587
Qutluq <kootlook.applications@gmail.com>
2022-10-03 23:04:08 +0600

    feat: added login page

---
+++ src/components/login/index.tsx
@@ -0,0 +1,3 @@
+import { Login } from 'src/components/login/login'
+
+export { Login }
---
+++ src/pages/login.tsx
@@ -0,0 +1,7 @@
+import { Login as LoginLayout } from 'src/components/login'
+
+export const Login = () => {
+  return <LoginLayout />
+}
+
+export default Login
